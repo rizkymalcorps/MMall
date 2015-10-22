3fe2c7d0b9837e9088f4fb29499e6cc6d0933b51
rizkymalcorps <rizkymalcorps@gmail.com>
2015-10-22 12:10:39 +0700

    Update admin.py

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+class CartItemAdmin(admin.TabularInline):
+    model = CartItem
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemAdmin, ]
+
+# Register your models here.
 admin.site.register(Brand)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Cart,CartAdmin)
